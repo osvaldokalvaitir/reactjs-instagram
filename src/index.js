e7953cfc0c552d069ddcebf7cb214fc28e7cac47
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-06-16 14:29:42 -0300

    Conclusão do aplicativo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import "./global.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
